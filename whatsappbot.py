6004f3629111377a026e46b364bd40e1d84d3aa6
AJITH KUMAR M <89124776+ajitharunai@users.noreply.github.com>
2021-11-12 11:05:07 +0530

    Add files via upload

---
+++ whatsappbot.py
@@ -0,0 +1,11 @@
+from email import message
+from types import CodeType
+import pywhatkit
+
+pywhatkit.sendwhatmsg(" +91 7010174493",
+                      "hello hi bro i'm Ajit kumar hacker ",
+                       9,53,10)
+#ts explain the CodeType
+#ndwhatsmsg(phone no:str,message:str,time_hour:int,time_min:int,wait_time:int=10)
+
+    
